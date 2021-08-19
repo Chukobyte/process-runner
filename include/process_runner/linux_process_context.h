9a4874997f097d13bbae4f17bbb47331ab503371
Chukobyte <chukobyte@gmail.com>
2021-08-18 20:25:45 -0400

    Linux (#1)
    Implemented linux process context.

--- include/process_runner/linux_process_context.h
+++ include/process_runner/linux_process_context.h
@@ -2,22 +2,81 @@
 #define LINUX_PROCESS_CONTEXT_H
 
 #include <iostream>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <vector>
+#include <cstring>
+#include <algorithm>
 
 class ProcessContext {
   private:
-    bool running = false;
+    pid_t pid;
 
-  public:
-    ProcessContext() {
-        std::cout << "Linux process context" << std::endl;
+    static std::vector<std::string> SplitString(const std::string s) {
+        if (s.empty()) {
+            return {};
+        }
+        std::vector<std::string> splitStrings;
+        std::string temp = "";
+        for(char i : s) {
+            if(i==' ') {
+                splitStrings.push_back(temp);
+                temp = "";
+            } else {
+                temp.push_back(i);
+            }
+        }
+        splitStrings.push_back(temp);
+        return splitStrings;
     }
 
-    void Start(const std::string &processPath, const std::string &startArgs) {}
+    std::vector<char*> GetFullArgs(const std::string &processPath, const std::string &startArgs) {
+        std::vector<char*> args;
+        const std::string fullStartArgs = startArgs.empty() ? processPath : processPath + " " + startArgs;
+        std::vector<std::string> splitStringArgs = SplitString(fullStartArgs);
+        std::transform(splitStringArgs.begin(), splitStringArgs.end(), std::back_inserter(args),
+        [](std::string &s) {
+            s.push_back(0);
+            return &s[0];
+        });
+        args.push_back(nullptr);
+        return args;
+    }
 
-    void Stop() {}
+  public:
+    void Start(const std::string &processPath, const std::string &startArgs) {
+        if (IsRunning()) {
+            return;
+        }
+        pid = fork();
+        if (pid < 0) {
+            std::cerr << "Error creating fork!" << std::endl;
+        } else if(pid > 0) {
+//            std::cout << "In parent process!" << std::endl;
+        } else {
+            std::vector<char*> args = GetFullArgs(processPath, startArgs);
+            execv(processPath.c_str(), args.data());
+        }
+    }
+
+    void Stop() {
+        if (!IsRunning()) {
+            return;
+        }
+        kill(pid, SIGUSR1);
+    }
 
     bool IsRunning() const {
-        return running;
+        while(waitpid(-1, 0, WNOHANG) > 0) {
+            // Wait for defunct....
+        }
+        if (kill(pid, 0) == 0) {
+            return true;
+        }
+        return false;
     }
 };
 
