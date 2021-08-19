129e75d6b158da87cfc69a382a9ad70ff1a7215c
chukobyte <chukobyte@gmail.com>
2021-08-18 20:13:37 -0400

    Clean.

--- include/process_runner/linux_process_context.h
+++ include/process_runner/linux_process_context.h
@@ -20,6 +20,24 @@ class ProcessContext {
         return pc;
     }
 
+    static std::vector<std::string> SplitString(const std::string s) {
+        if (s.empty()) {
+            return {};
+        }
+        std::vector<std::string> splitStrings;
+        std::string temp = "";
+        for(char i : s) {
+            if(i==' ') {
+                splitStrings.push_back(temp);
+                temp = "";
+            } else {
+                temp.push_back(i);
+            }
+        }
+        splitStrings.push_back(temp);
+        return splitStrings;
+    }
+
   public:
     void Start(const std::string &processPath, const std::string &startArgs) {
         if (IsRunning()) {
@@ -29,13 +47,22 @@ class ProcessContext {
         if (pid < 0) {
             std::cerr << "Error creating fork!" << std::endl;
         } else if(pid > 0) {
-//            std::cout << "In parent process!" << std::endl;
+            //            std::cout << "In parent process!" << std::endl;
         } else {
             const std::string fullStartArgs = startArgs.empty() ? processPath : processPath + " " + startArgs;
-            char *args[fullStartArgs.size()] = ConvertStringToChar(fullStartArgs);
+            std::vector<std::string> splitStringArgs = SplitString(fullStartArgs);
+            std::vector<char*> vec;
+            std::transform(splitStringArgs.begin(), splitStringArgs.end(), std::back_inserter(vec),
+            [](std::string &s) {
+                s.push_back(0);
+                return &s[0];
+            });
+            vec.push_back(nullptr);
+            char** args = vec.data();
             execv(processPath.c_str(), args);
-//            char *args[] = {processPath.c_str(), "-lh", "/home", nullptr};
-//            execl(processPath.c_str(), processPath.c_str(), nullptr);
+//            char *args[fullStartArgs.size()] = ConvertStringToChar(fullStartArgs);
+            //            char *args[] = {processPath.c_str(), "-lh", "/home", nullptr};
+            //            execl(processPath.c_str(), processPath.c_str(), nullptr);
         }
     }
 
